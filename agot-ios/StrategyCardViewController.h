8719d0cf524fe37a4dad243a5a7d761ce7281104
mebusw <mebusw@163.com>
2012-02-26 22:08:46 +0800

    add StrategyCardViewController

---
+++ agot-ios/StrategyCardViewController.h
@@ -0,0 +1,21 @@
+//
+//  StrategyCardViewController.h
+//  agot-ios
+//
+//  Created by mebusw on 12-2-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StrategyCardViewController : UIViewController
+
+
+@property (nonatomic, strong) IBOutlet UILabel *lblCardTitle;
+@property (nonatomic, strong) IBOutlet UILabel *lblCardRules;
+@property (nonatomic, strong) IBOutlet UILabel *lblCardSet;
+@property (nonatomic, strong) IBOutlet UILabel *lblCardGinc;
+@property (nonatomic, strong) IBOutlet UILabel *lblCardIni;
+@property (nonatomic, strong) IBOutlet UILabel *lblCardClaim;
+
+@end
